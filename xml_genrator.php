fc0f04ebe9a82a66cf2143691455f0c9844dd1c6
cnngraphics <cnngraphics@gmail.com>
2015-05-14 06:18:40 -0400

    Create XML file from Database
    Simple script to just connect to your database, and generate XML files from tables.Just change database and table settings and run the script to generate XML file.

---
+++ xml_genrator.php
@@ -0,0 +1,45 @@
+<?php
+$xml = new SimpleXMLElement('<xml/>');
+//IMPORTANT: this document must start with the line above, otherwise: ERROR!
+
+include '/pages/dbconnect.php'; 
+
+/*that file (dbconnect) should have your database connection settings 
+**or just create a database connection handle here.
+**my database connection here is called "$conn"
+**/
+
+//print_r($conn); //for debugging
+
+
+$stmt= $conn->query('Select * from dbtable'); 
+//replace with your table name or custom query
+
+
+
+while ($row=$stmt->fetch(PDO::FETCH_ASSOC )){
+		$id=$row['id'];
+		$territoireID = $row['territoireId'];
+		$city=$row['city'];
+		$zip=$row['zipCode'];
+/**/my table had these columns: id, territoireID, city, ZIP....replace these with yours
+**some programmer might create abstract script to pull your tables automagically. 
+**This script is simple and clean
+**/
+
+		$territoire = $xml->addChild('Territoire'); //create an XML root object and expand on it
+		
+		$territoire->addChild('id',$id);
+		$territoire->addChild('territoireId', $territoireID);
+		$territoire->addChild('territoireId', $id);
+		$territoire->addChild('city', $city);
+		$territoire->addChild('zip', $zip);
+		
+			
+}
+
+//generate XML file
+
+Header('Content-type: text/xml');
+print($xml->asXML());
+
